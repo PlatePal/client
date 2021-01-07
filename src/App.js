197fd316c5fffa8d6ca88856356fb814f1c66ed1
jhwan7 <46652246+jhwan7@users.noreply.github.com>
2021-01-06 21:45:40 -0500

    add metrics page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import CreateAccountPage from "./Containers/Pages/CreateAccount/CreateAccount";
 import LoginPage from "./Containers/Pages/Login/Login";
 import LogoPage from "./Containers/Pages/Logo/Logo";
+import MetricPage from "./Containers/Pages/Metrics/Metrics";
 import { Route, Switch } from "react-router-dom";
 function App() {
   return (
     <div>
       <Switch>
-        <Route path="/create" component={CreateAccountPage} />
-        <Route path="/login" component={LoginPage} />
-        <Route path="/" exact component={LogoPage} />
+        {/* <Route path="/create" component={CreateAccountPage} />
+        <Route path="/login" component={LoginPage} /> */}
+        {/* <Route path="/" exact component={LogoPage} /> */}
+        <Route path="/" exact component={MetricPage} />
       </Switch>
     </div>
   );
