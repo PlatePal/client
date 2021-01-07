73b41e2e7b3e8527050fed13cdd2c536bb047004
jhwan7 <46652246+jhwan7@users.noreply.github.com>
2021-01-06 19:11:08 -0500

    create initial pages & remove redundant files

---
+++ src/Containers/Pages/CreateAccount/CreateAccount.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./CreateAccount.module.css";
+class CreateAccount extends React.Component {
+  render() {
+    return <div className={styles.text}>CreateAccount</div>;
+  }
+}
+
+export default CreateAccount;
