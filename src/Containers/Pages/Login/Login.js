9ff3cd2fc6923ddba154fd844cc29491bc9edb13
julia-999 <janantch@uwo.ca>
2021-01-17 03:55:43 -0500

    google sign in
    replaced text boxes with a button that will sign you in with google once I figure out firebase

--- src/Containers/Pages/Login/Login.js
+++ src/Containers/Pages/Login/Login.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, {useState} from 'react';
+import './Login.css';
+import logo from "./googleicon.png";
 
 class Login extends React.Component {
-  render() {
-    return <div>Login</div>;
-  }
+unin
+    state = {
+        signedIn: false,
+        user: null,
+
+    }
+
+    googleSignIn () {
+        console.log("Google Sign In Page");
+    }
+
+    helpNeeded () {
+        console.log("Help is needed");
+    }
+
+    render () {
+        return(
+            <div className="loginComponent">
+                <h1 className="loginTitle">Login</h1>
+                <p className="loginParagraph">Welcome Back</p>
+                <p className="loginParagraph">Sign in to continue</p>
+                <img className="loginLogo" src={logo} alt="Google Logo"></img>
+                <button className="loginButton" onClick={this.googleSignIn}>Sign in with Google</button>
+                <p className="loginParagraphOrange" onClick={this.helpNeeded}>Need Help?</p>
+            </div>
+        );
+    }
 }
 
 export default Login;
