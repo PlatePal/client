e11339e7cc849f804e2bc4ed5a0a69fffbb72a41
julia-999 <janantch@uwo.ca>
2021-01-23 18:08:15 -0500

    Created main recipe page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { Route, Switch } from "react-router-dom";
 import 'fontsource-roboto';
 import DietaryRestrictions from "./Containers/Pages/DietaryRestrictions/DietaryRestrictions.js";
 import {createMuiTheme, ThemeProvider} from "@material-ui/core";
+import MainRecipeModal from "./Containers/Pages/MainRecipeModal/MainRecipeModal";
+import smoothie from "./Containers/Pages/MainRecipeModal/Creamy-Watermelon-Smoothie.jpg"
 
 const theme = createMuiTheme({
   palette: {
@@ -27,6 +29,17 @@ function App() {
         <Route path="/" exact component={LandingPage} />
         <Route path="/metric" exact component={MetricPage} />
         <Route path="/dietaryrestrictions" exact component={DietaryRestrictions} />
+        <Route 
+          path="/mainrecipe" 
+          render={(props) => (
+            <MainRecipeModal 
+              name="Creamy Watermelon Smoothie" 
+              percentage="87%"
+              price="$"
+              img={smoothie} 
+            />
+          )}
+        />
       </Switch>
     </ThemeProvider>
   );
