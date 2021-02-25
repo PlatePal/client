4d476fe50040bfcb570265f026a355722dd5b381
Ewhan Han <ewhan.han@gmail.com>
2021-02-24 19:03:20 -0500

    Fixed small stuff

--- src/component/CompressedRecipe/CompressedRecipe.js
+++ src/component/CompressedRecipe/CompressedRecipe.js
@@ -7,18 +7,15 @@ const CompressedRecipe = (props) => {
   return (
     <div className={styles.recipeContainer}>
       <div className={styles.recipeInfoContainer}>
-        <h1 className={styles.recipeName}>Mushroom Pasta with Spinach </h1>
+        <h2 className={styles.recipeName}>{props.recipeName} </h2>
         <div className={styles.recipeQuickSummaryContainer}>
-          <CostSummary pricePerServing="10" />
-          <CookingTimeSummary readyInMinutes="16" />
+          <CostSummary pricePerServing={props.pricePerServing} />
+          <CookingTimeSummary readyInMinutes={props.readyInMinutes} />
+          <CostSummary pricePerServing={props.pricePerServing} />
         </div>
       </div>
       <div className={styles.recipeImageContainer}>
-        <img
-          className={styles.recipeImage}
-          src="https://spoonacular.com/cdn/ingredients_100x100/apple.jpg"
-          alt="recipe"
-        ></img>
+        <img className={styles.recipeImage} src={props.recipeImageSrc} alt="recipe"></img>
       </div>
     </div>
   );
