503ed7d7570d9ef178da0859181f537b282962d6
Ewhan Han <ewhan.han@gmail.com>
2021-02-26 11:49:46 -0500

    Changed figure & figcaption to div & p respectively

--- src/component/CompressedRecipe/QuickSummaryItems/CookingTimeSummary/CookingTimeSummary.js
+++ src/component/CompressedRecipe/QuickSummaryItems/CookingTimeSummary/CookingTimeSummary.js
@@ -3,14 +3,14 @@ import styles from '../QuickSummary.module.scss';
 
 const CookingTimeSummary = (props) => {
   return (
-    <figure className={styles.QuickSummaryContainer}>
+    <div className={styles.QuickSummaryContainer}>
       <img
         className={styles.QuickSummaryIcon}
         src="https://spoonacular.com/application/frontend/images/badges/fast.svg"
         alt="time to cook"
       ></img>
-      <figcaption>{props.readyInMinutes} Minutes</figcaption>
-    </figure>
+      <p>{props.readyInMinutes} Minutes</p>
+    </div>
   );
 };
 
--- src/component/CompressedRecipe/QuickSummaryItems/CostSummary/CostSummary.js
+++ src/component/CompressedRecipe/QuickSummaryItems/CostSummary/CostSummary.js
@@ -3,14 +3,14 @@ import styles from '../QuickSummary.module.scss';
 
 const CostSummary = (props) => {
   return (
-    <figure className={styles.QuickSummaryContainer}>
+    <div className={styles.QuickSummaryContainer}>
       <img
         className={styles.QuickSummaryIcon}
         src="https://spoonacular.com/application/frontend/images/badges/cheap.svg"
         alt="cost"
       ></img>
-      <figcaption>${props.pricePerServing}</figcaption>
-    </figure>
+      <p>${props.pricePerServing}</p>
+    </div>
   );
 };
 
