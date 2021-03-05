5f88c38c8af5af5589fb68c6bfbe3c5cfd37d410
Ewhan Han <ewhan.han@gmail.com>
2021-03-05 10:42:46 -0500

    Axios and API connection to Main Modal page

---
+++ src/utilities/API/Spoonacular.js
@@ -0,0 +1,8 @@
+const api_key = '&apiKey=5e05f1c45b9b42488369cc8c8cbe13e3';
+export const API_KEY = '5e05f1c45b9b42488369cc8c8cbe13e3';
+
+const SpoonacularAPI = {
+  GET_RANDOM: '/recipes/random/',
+};
+
+export default SpoonacularAPI;
