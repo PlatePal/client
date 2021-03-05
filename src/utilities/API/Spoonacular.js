b39c827f39059ca01011cc9a20cb049a3d0dcd92
Ewhan Han <ewhan.han@gmail.com>
2021-03-05 18:45:06 -0500

    moved API key to process.env file

--- src/utilities/API/Spoonacular.js
+++ src/utilities/API/Spoonacular.js
@@ -1,5 +1,4 @@
-const api_key = '&apiKey=5e05f1c45b9b42488369cc8c8cbe13e3';
-export const API_KEY = '5e05f1c45b9b42488369cc8c8cbe13e3';
+export const API_KEY = process.env.REACT_APP_SPOONACULAR_API_KEY;
 
 const SpoonacularAPI = {
   GET_RANDOM: '/recipes/random/',
