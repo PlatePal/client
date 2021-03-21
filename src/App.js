7c17dd2736e731a096b70410f0305c84063f1b4d
jhwan7 <46652246+jhwan7@users.noreply.github.com>
2021-03-21 04:37:16 -0400

    populate expanded recipe page

--- src/App.js
+++ src/App.js
@@ -1,53 +1,23 @@
-import { Route, Switch, BrowserRouter } from "react-router-dom";
+import { Route, Switch, BrowserRouter, Redirect } from "react-router-dom";
 
-import DietaryRestrictions from "./Containers/Pages/DietaryRestrictions/DietaryRestrictions.js";
-import LoginPage from "./Containers/Pages/Login/Login";
 import LandingPage from "./Containers/Pages/LandingPage/LandingPage";
-import MetricPage from "./Containers/Pages/Metrics/Metrics";
 import MainRecipeModal from "./Containers/Pages/MainRecipeModal/MainRecipeModal";
-import ExpandedSavedRecipe from "./Containers/Pages/ExpandedSavedRecipe/ExpandedSavedRecipe.js";
-import ExpandedRecipe from "./Containers/Pages/ExpandedRecipe/ExpandedRecipe.js";
-import IngredientSelection from "./Containers/Pages/IngredientSelection/IngredientSelection.js";
 import ProfilePage from "./Containers/Pages/ProfilePage/ProfilePage.js";
-import AdjustOptions from "./Containers/Pages/AdjustOptions/AdjustOptions.js";
 import TodaysRecipe from "./Containers/Pages/TodaysRecipe/TodaysRecipe";
+import SavedRecipePage from "./Containers/Pages/SavedRecipePage/SavedRecipePage.js";
+import Navbar from "./component/navbars/Navbar.js";
 
 import { ThemeProvider } from "@material-ui/core";
 import mainUITheme from "./assets/styles/mainUITheme";
 import "fontsource-roboto";
-
 import routes from "./constant/routes";
-import Navbar from "./component/navbars/Navbar.js";
-import SavedRecipePage from "./Containers/Pages/SavedRecipePage/SavedRecipePage.js";
+import ExpandedSavedRecipe from "./Containers/Pages/ExpandedSavedRecipe/ExpandedSavedRecipe";
 
 function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={mainUITheme}>
         <Switch>
-          <Route path={`/${routes.login}`} component={LoginPage} />
-          <Route path="/" exact component={LandingPage} />
-          <Route path={`/${routes.metrics}`} exact component={MetricPage} />
-          <Route
-            path={`/${routes.dietaryRestriction}`}
-            exact
-            component={DietaryRestrictions}
-          />
-          <Route
-            path={`/${routes.expandedSavedRecipe}`}
-            exact
-            component={ExpandedSavedRecipe}
-          />
-          <Route
-            path={`/${routes.expandedRecipe}`}
-            exact
-            component={ExpandedRecipe}
-          />
-          <Route
-            path={`/${routes.ingredientSelection}`}
-            exact
-            component={IngredientSelection}
-          />
           <Route
             path={`/${routes.main}`}
             render={(props) => (
@@ -65,12 +35,6 @@ function App() {
                 <TodaysRecipe />
               </div>
             )}
-          />
-
-          <Route
-            path={`/${routes.adjustOptions}`}
-            exact
-            component={AdjustOptions}
           />
           <Route
             path={`/${routes.profile}`}
@@ -90,6 +54,17 @@ function App() {
               </div>
             )}
           />
+          <Route
+            path={`/${routes.expandedRecipe}`}
+            render={(props) => (
+              <div>
+                <Navbar fill="home" />
+                <ExpandedSavedRecipe />
+              </div>
+            )}
+          />
+          <Redirect from="/:random" to="/" />
+          <Route path="/" component={LandingPage} />
         </Switch>
       </ThemeProvider>
     </BrowserRouter>
